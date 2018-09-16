60a0c57dcef61c9472350116b7450947b8f2abf9
dreamhe <dreamhe.eng@gmail.com>
2018-09-16 22:50:05 +0800

    （修改）增加选择排序

---
+++ AlgorithmStudy/src/com/hlx/test/SelectSortArrayTest.java
@@ -0,0 +1,30 @@
+package com.hlx.test;
+
+import com.hlx.array.SelectSortArray;
+
+public class SelectSortArrayTest {
+	public static void main(String[] args) {
+		SelectSortArray array = new SelectSortArray(10);
+		
+		array.insert(12);
+		array.insert(56);
+		array.insert(9);
+		array.insert(454);
+		array.insert(987);
+		array.insert(1245);
+		array.insert(654);
+		array.insert(45);
+		array.insert(6);
+		array.insert(100);
+		
+		array.display();
+		
+		array.selectSort();
+		
+		array.display();
+		
+		System.out.println(array.find(12));
+		System.out.println(array.find(13));
+		
+	}
+}
